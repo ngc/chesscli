d1b0ca9d1fc73fc083c913299f99044b37b3777d
thaniel-c <contact.nathancoulas@gmail.com>
2020-07-09 13:50:50 -0400

    bishop movement

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int printCell(int val){
 	return 0;
 }
 
-bool checkMove(string command){
+bool checkMove(string command, int side = 1){
 	if(command.length() != 6) return false;
 	
 	int r1 = command[1] - '0' - 1;
@@ -58,6 +58,7 @@ bool checkMove(string command){
 	int c2 = abs(command[4] - 'H');
 	
 	if(r1 == r2 && c1 == c2) return false;
+	if(abs(board[r2][c2] == 5)) return false;
 	if(c2 > 7 || c2 < 0 || c1 > 7 || c1 < 0 || r2 > 7 || r2 < 0 || r1 > 7 || r1 < 0) return false;
 	
 	//cout << "|" << r1 << "." << c1 << "\\" << "|" << r2 << "." << c2 << "\\" << "\n";
@@ -66,14 +67,14 @@ bool checkMove(string command){
 		case 1: /*♖*/ 
 		if((r1 == r2 && c1 != c2) || (c1 == c2 && r1 != r2)){
 			if(c2 == c1){
-				for(int j = 0; j < abs(r2 - r1); j++){
+				for(int j = 1; j < abs(r2 - r1); j++){
 					int cur = j * (((r1 > r2) * -1) + (r1 < r2));
-					cout << "SEARCHING: ROW: " << r1 + cur << " COL: " << c2 << " | " << printCell(board[r1 + cur][c1]) << "\n";
-					if(board[r1 + cur][c1] != 0 && cur != 0) return false;
+			//		cout << "SEARCHING: ROW: " << r1 + cur << " COL: " << c2 << " | " << printCell(board[r1 + cur][c1]) << "\n";
+					if(board[r1 + cur][c1] != 0) return false;
 				}
 			}else{
-				for(int j = 0; j < abs(c2 - c1); j++){
-					int cur = j * ((c1 > c2 * -1) + c1 < c2);
+				for(int j = 1; j < abs(c2 - c1); j++){
+					int cur = j * (((c1 > c2) * -1) + (c1 < c2));
 					cout << printCell(board[r1][c1 + cur]) << "\n";
 					if(board[r1][c1 + cur] != 0) return false;
 				}	
@@ -81,17 +82,48 @@ bool checkMove(string command){
 			board[r1][c1] = 0;
 			board[r2][c2] = 1;
 			return true;
-		}else{
-			return false;
 		}
 		
 		case 2: cout << "♘︎"; break;
-		case 3: cout << "♗︎"; break;
+		case 3: /*♗*/
+			if(abs(r2 - r1) == abs(c2 - c1)){
+				
+			if(c2 == c1){
+				for(int j = 1; j < abs(r2 - r1); j++){
+					int cur = j * (((r1 > r2) * -1) + (r1 < r2));
+					if(board[r1 + cur][c1] != 0) return false;
+				}
+			}else{
+				for(int j = 1; j < abs(c2 - c1); j++){
+					int cur = j * (((c1 > c2) * -1) + (c1 < c2));
+					cout << printCell(board[r1][c1 + cur]) << "\n";
+					if(board[r1][c1 + cur] != 0) return false;
+				}	
+			}
+				board[r1][c1] = 0;
+				board[r2][c2] = 3;
+				return true;
+			}
 		case 4: cout << "♕︎"; break;
 		case 5: cout << "♔"; break;
-		case 6: cout << "♙"; break;
+		case 6: /*♙ PAWN: Check if on players initial row, if so then permit a movement of 2 if not permit a movement of 1*/
+		if(c2 == c1){
+			if(r2 - r1 == -1 && board[r2][c2] == 0){
+				board[r1][c1] = 0;
+				board[r2][c2] = 6;
+				return true;
+			}else if(r1 == 6 && r2 - r1 == -2 && board[r2][c2] == 0){
+				board[r1][c1] = 0;
+				board[r2][c2] = 6;
+				return true;
+			}
+		}else if(abs(c2 - c1) == 1 && r2 - r1 == -1 && (board[r2][c2] / side) < 0){
+			board[r1][c1] = 0;
+			board[r2][c2] = 6;
+			return true;
+		}
 		
-		case 0: cout << "⠀"; break;
+		case 0: return false;
 		
 		case -1: cout << "♜"; break;
 		case -2: cout << "♞︎"; break;
@@ -101,7 +133,7 @@ bool checkMove(string command){
 		case -6: cout << "♟︎"; break;
 	}
 	
-	return true;
+	return false;
 }
 
 int PrintBoard(){
