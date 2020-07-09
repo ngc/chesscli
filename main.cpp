4583f4618fd335332a7017bcd572809dd5c43367
thaniel-c <contact.nathancoulas@gmail.com>
2020-07-09 18:00:48 -0400

    bishop finished

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int board[8][8] = {
 	-6, -6, -6, -6, -6, -6, -6, -6,
 	0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 3, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0,
 	6, 6, 6, 6, 6, 6, 6, 6,
 	1, 2, 3, 4, 5, 3, 2, 1,
@@ -52,30 +52,28 @@ int printCell(int val){
 bool checkMove(string command, int side = 1){
 	if(command.length() != 6) return false;
 	
-	int r1 = command[1] - '0' - 1;
-	int c1 = abs(command[0] - 'H');
-	int r2 = command[5] - '0' - 1;
-	int c2 = abs(command[4] - 'H');
+	int r1 = 7 - (command[1] - '0' - 1);
+	int c1 = abs(command[0] - 'A');
+	int r2 = 7 - (command[5] - '0' - 1);
+	int c2 = abs(command[4] - 'A');
+	
+	//cout << "|" << r1 << "." << c1 << "\\" << "|" << r2 << "." << c2 << "\\" << "\n";
 	
 	if(r1 == r2 && c1 == c2) return false;
 	if(abs(board[r2][c2] == 5)) return false;
 	if(c2 > 7 || c2 < 0 || c1 > 7 || c1 < 0 || r2 > 7 || r2 < 0 || r1 > 7 || r1 < 0) return false;
 	
-	//cout << "|" << r1 << "." << c1 << "\\" << "|" << r2 << "." << c2 << "\\" << "\n";
-	
-	switch(board[r1][c1]){
+	switch(board[r1][c1] / side){
 		case 1: /*♖*/ 
 		if((r1 == r2 && c1 != c2) || (c1 == c2 && r1 != r2)){
 			if(c2 == c1){
 				for(int j = 1; j < abs(r2 - r1); j++){
 					int cur = j * (((r1 > r2) * -1) + (r1 < r2));
-			//		cout << "SEARCHING: ROW: " << r1 + cur << " COL: " << c2 << " | " << printCell(board[r1 + cur][c1]) << "\n";
 					if(board[r1 + cur][c1] != 0) return false;
 				}
 			}else{
 				for(int j = 1; j < abs(c2 - c1); j++){
 					int cur = j * (((c1 > c2) * -1) + (c1 < c2));
-					cout << printCell(board[r1][c1 + cur]) << "\n";
 					if(board[r1][c1 + cur] != 0) return false;
 				}	
 			}
@@ -84,7 +82,16 @@ bool checkMove(string command, int side = 1){
 			return true;
 		}
 		
-		case 2: cout << "♘︎"; break;
+		case 2: /*♘*/
+			switch(abs(r2 - r1)){
+				case  1: if(abs(c2 - c1) != 2) return false;
+				case  2: if(abs(c2 - c1) != 1) return false;
+			}
+			
+			board[r1][c1] = 0;
+			board[r2][c2] = 2;
+			return true;
+		
 		case 3: /*♗*/
 			if(abs(r2 - r1) == abs(c2 - c1) && (board[r2][c2] / side) <= 0){
 				for(int i = 1; i < abs(c2 - c1); i++){
@@ -130,13 +137,13 @@ bool checkMove(string command, int side = 1){
 int PrintBoard(){
 	system("clear");
 	for(int i = 0; i < 8; i++){
-		cout << i + 1 << " ";
+		cout << 8 - i << " ";
 		for(int j = 0; j < 8; j++){
 			printCell(board[i][j]);
 		}
 		cout << "\n";
 	}
-	cout << "• H G F E D C B A\n";
+	cout << "• A B C D E F G H\n";
 	return 0;
 }
 
@@ -159,7 +166,7 @@ string pause() {
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cout << "Example command: H7 -> H5 \n";
+	cout << "Example command: A7 -> A5 \n";
 	while(true){
 		PrintBoard();
 		cout << "Command: ";
