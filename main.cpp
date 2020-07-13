cad1b306818e7670e821073d582d17f35cbdab53
thaniel-c <contact.nathancoulas@gmail.com>
2020-07-13 00:44:53 -0400

    help menu

--- main.cpp
+++ main.cpp
@@ -8,7 +8,32 @@ using namespace std;
 int main(int argc, char** argv){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	
-	localMode();
+	if(argc > 2){
+		cout << "Too many arguments, --help for a list of possible arguments.\n";
+		return 0;
+	}if(argc == 2){
+		string argument = argv[1];
+		if(argument == "--help" || argument == "-h"){
+			cout << R"(
+ ██████ ██   ██ ███████ ███████ ███████  ██████ ██      ██ 
+██      ██   ██ ██      ██      ██      ██      ██      ██ 
+██      ███████ █████   ███████ ███████ ██      ██      ██ 
+██      ██   ██ ██           ██      ██ ██      ██      ██ 
+ ██████ ██   ██ ███████ ███████ ███████  ██████ ███████ ██     
+                                                                  
+)";
+			cout << "ChessCLI Help Menu:\n";
+			cout << "   --help or -h brings up this help menu.\n";
+			cout << "   --local or no arguments starts a local match with two players taking turns controlling the same terminal.\n";
+			cout << "   --ssh %IP %PORT(Optional) starts an online multiplayer match with another player.\n";
+			cout << "   --cpu or -c starts a match with Stockfish.\n";
+			cout << "   --maxis or -m starts a match with Maxis.\n";
+		}
+		
+		return 0;
+	}else{
+		localMode();
+	}
 	
 	return 0;
 }
